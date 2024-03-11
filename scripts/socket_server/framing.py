a953ed86f6c82ab01c7b3139e7dfaa4ba7e1d297
Viacheslav <slava.k@ks2corp.com>
2024-03-11 20:48:14 +0300

    added framing test out

--- scripts/socket_server/framing.py
+++ scripts/socket_server/framing.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import chunk
 import ctypes as ct
 from dataclasses import dataclass
 
@@ -16,6 +17,8 @@ class CunkLL(ct.Structure):
         ('crc',      ct.c_uint32), # контрольная сумма payload
         # ('reserved', ct.c_uint8), # данные
     ]
+
+PAYLOAD_MAX_IN_CHUNK = 128 - ct.sizeof(CunkLL)
 
 @dataclass
 class Chunk:
@@ -45,7 +48,37 @@ class Chunk:
                      crc=ll.crc,
                      payload=data[12:])
 
-def main():
+    @staticmethod
+    def to_buffer(channel:int, data: bytes) -> list['Chunk']:
+        payloads = [
+            data[i:i + PAYLOAD_MAX_IN_CHUNK]
+            for i in range(0, len(data), PAYLOAD_MAX_IN_CHUNK)
+        ]
+        chunks = []
+        # total = 1 if len(payloads) <= PAYLOAD_MAX_IN_CHUNK else len(payloads)
+        for i, p in enumerate(payloads):
+            chunks.append(
+                Chunk(channel=channel,
+                      size=len(p),
+                      number=i,
+                      total=len(payloads),
+                      crc=0x1234_4321,
+                      payload=p))
+        return chunks
+
+def test_out():
+    TEST_DATA = b'Hello World'
+    LONG_DATA = b'Hello World' * 20
+
+    chunks = Chunk.to_buffer(0x11, TEST_DATA)
+    print(chunks)
+
+    print('-' * 10)
+
+    chunks = Chunk.to_buffer(0x11, LONG_DATA)
+    print(chunks)
+
+def test_in():
     TEST_DATA = b'\x02\x11\x02\x06\x01\x01\x02\x01\x0b\xff\xff\xff\xff\x02 D000\x00\x02\x11\x02\n\x01\x01\x02\x01\x0f\xff\xff\xff\xff0\x1c95bfc1f\x1c\x00\x02\x11\x02\x03\x01\x01\x02\x01\x08\xff\xff\xff\xff\x035B\x00'
 
     chunks_raw = [c for c in TEST_DATA.split(b'\x00') if len(c)]
@@ -57,6 +90,10 @@ def main():
     print(chunk_raw)
     chunk = Chunk.from_buffer(chunk_raw)
     print(chunk)
+
+def main():
+    test_out()
+    # test_in()
 
 if __name__ == "__main__":
     main()
