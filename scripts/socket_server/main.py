4e9a5356275006dcf196d795be1a738736025598
Viacheslav <slava.k@ks2corp.com>
2024-03-17 10:59:06 +0300

    cleanup

--- scripts/socket_server/main.py
+++ scripts/socket_server/main.py
@@ -5,6 +5,7 @@ import time
 
 import pylogus
 import serial.tools.list_ports
+from colorama import Fore as Clr
 from connector import HwConnector
 from konn_proxy import KonnProxy, ServiceSocketConfig
 
@@ -17,9 +18,9 @@ def main():
     for port in serial.tools.list_ports.comports():
         log.info(port)
 
-    configs = (ServiceSocketConfig(channel=0, host="127.0.0.1", port=9000),
-               ServiceSocketConfig(channel=1, host="127.0.0.1", port=9001),
-               ServiceSocketConfig(channel=2, host="127.0.0.1", port=9002))
+    configs = (ServiceSocketConfig(channel=0, host="192.168.0.111", port=9000),
+               ServiceSocketConfig(channel=1, host="192.168.0.111", port=9001),
+               ServiceSocketConfig(channel=2, host="192.168.0.111", port=9002))
     konn = KonnProxy(hwport=HwConnector(SERIAL_PORT), configs=configs)
 
     try:
@@ -28,7 +29,7 @@ def main():
     except KeyboardInterrupt:
         pass
     konn.disconnect()
-    log.info("Kill all")
+    log.info(f"{Clr.YELLOW}Kill all{Clr.RESET}")
 
 
 if __name__ == "__main__":
