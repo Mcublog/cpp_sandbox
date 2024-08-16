2ff0f7a494f02f57d4df0a48652525b9dd87d688
Viacheslav <slava.k@ks2corp.com>
2024-08-16 10:15:41 +0300

    added new target

---
+++ qmeter/main.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file main.cpp
+ * @author Viacheslav (viacheslav@mcublog.ru)
+ * @brief
+ * @version 0.1
+ * @date 2024-08-16
+ *
+ * @copyright Mcublog Copyright (c) 2024
+ *
+ */
+//>>---------------------- Log control
+#define LOG_MODULE_NAME     qmeter
+#define LOG_MODULE_LEVEL    (4)
+#include "libs/debug/log_libs.h"
+//<<----------------------
+
+/**
+ * @brief
+ *
+ */
+int main(void)
+{
+    LOG_INFO("hello");
+    return 0;
+}
