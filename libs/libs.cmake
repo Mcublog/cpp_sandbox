ec26ab3dd53b3d9e385829869d34403ac134f3fd
Viacheslav <viacheslav@mcublog.ru>
2025-06-30 09:51:32 +0300

    added alocator target;

--- libs/libs.cmake
+++ libs/libs.cmake
@@ -2,3 +2,7 @@ file(GLOB_RECURSE CMETER_SOURCES
     ${PROJECT_SOURCE_DIR}/libs/CapacityMeter/*.c
     ${PROJECT_SOURCE_DIR}/libs/CapacityMeter/*.cpp
 )
+
+file(GLOB SMACLLOC_SOURCES
+    ${PROJECT_SOURCE_DIR}/libs/smalloc/*.c
+)
