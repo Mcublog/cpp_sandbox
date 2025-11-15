044ff0efa845b9ffef3ee7bf15a6fa89f3be56a4
Viacheslav <viacheslav@mcublog.ru>
2025-11-15 18:36:47 +0300

    imgui opengl2 backend;

--- libs/libs.cmake
+++ libs/libs.cmake
@@ -6,3 +6,16 @@ file(GLOB_RECURSE CMETER_SOURCES
 file(GLOB SMACLLOC_SOURCES
     ${PROJECT_SOURCE_DIR}/libs/smalloc/*.c
 )
+
+set(DEARIMGUI_SOURCES
+    ${PROJECT_SOURCE_DIR}/libs/imgui/imgui.cpp
+    ${PROJECT_SOURCE_DIR}/libs/imgui/imgui_tables.cpp
+    ${PROJECT_SOURCE_DIR}/libs/imgui/imgui_draw.cpp
+    ${PROJECT_SOURCE_DIR}/libs/imgui/imgui_widgets.cpp
+    ${PROJECT_SOURCE_DIR}/libs/imgui/backends/imgui_impl_sdl2.cpp
+)
+
+set(DEARIMGUI_INCLUDES
+    ${PROJECT_SOURCE_DIR}/libs/imgui
+    ${PROJECT_SOURCE_DIR}/libs/imgui/backends
+)
