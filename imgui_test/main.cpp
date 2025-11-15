460d54f07fd51eb1a67781f756bed0e42b777a52
Viacheslav <viacheslav@mcublog.ru>
2025-11-15 21:00:45 +0300

    imguitest: using x11 backend;

--- imgui_test/main.cpp
+++ imgui_test/main.cpp
@@ -1,159 +1,437 @@
-/**
- * @file main.cpp
- * @author Viacheslav (viacheslav@mcublog.ru)
- * @brief
- * @version 0.1
- * @date 2025-11-15
- *
- * @copyright Mcublog Copyright (c) 2025
- *
- */
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <GL/glx.h>
+#include <GL/glxext.h>
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
 
 #include "imgui.h"
-#include "imgui_impl_opengl2.h"
-#include "imgui_impl_sdl2.h"
-#include <SDL.h>
-#include <SDL_opengl.h>
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_x11.h"
 #include <stdio.h>
-#ifdef _WIN32
-#include <windows.h> // SetProcessDPIAware()
-#endif
 
-// Main code
-int main(int, char **)
+
+#include <string>
+
+extern int ImGui_ImplX11_EventHandler(XEvent &event);
+
+#define GLX_CONTEXT_MAJOR_VERSION_ARB       0x2091
+#define GLX_CONTEXT_MINOR_VERSION_ARB       0x2092
+typedef GLXContext (*glXCreateContextAttribsARBProc)(Display*, GLXFBConfig, GLXContext, Bool, const int*);
+
+static Display* g_display;
+static Window g_window;
+static Colormap g_colorMap;
+static GLXFBConfig g_bestFbConfig;
+static GLXContext g_glxContext = 0;
+static Atom g_wmDeleteMessage;
+static bool g_ctxErrorOccurred = false;
+
+void FindBestFrameBuffer();
+void CreateX11Window();
+void CreateGLXContext();
+void DestroyGLXContext();
+void DestroyX11Window();
+
+static bool isExtensionSupported(const char* extList, const char* extension)
 {
-    // Setup SDL
-#ifdef _WIN32
-    ::SetProcessDPIAware();
-#endif
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
-    {
-        printf("Error: %s\n", SDL_GetError());
-        return 1;
+    const char* start;
+    const char* where, * terminator;
+
+    /* Extension names should not have spaces. */
+    where = strchr(extension, ' ');
+    if (where || *extension == '\0')
+        return false;
+
+    /* It takes a bit of care to be fool-proof about parsing the
+       OpenGL extensions string. Don't be fooled by sub-strings,
+       etc. */
+    for (start = extList;;) {
+        where = strstr(start, extension);
+
+        if (!where)
+            break;
+
+        terminator = where + strlen(extension);
+
+        if (where == start || *(where - 1) == ' ')
+            if (*terminator == ' ' || *terminator == '\0')
+                return true;
+
+        start = terminator;
     }
 
-    // From 2.0.18: Enable native IME.
-#ifdef SDL_HINT_IME_SHOW_UI
-    SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
-#endif
+    return false;
+}
 
-    // Setup window
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-    float main_scale = ImGui_ImplSDL2_GetContentScaleForDisplay(0);
-    SDL_WindowFlags window_flags =
-        (SDL_WindowFlags)(SDL_WINDOW_OPENGL /*| SDL_WINDOW_RESIZABLE*/ |
-                          SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Window *window = SDL_CreateWindow(
-        "Dear ImGui SDL2+OpenGL example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        (int)(250 * main_scale), (int)(440 * main_scale), window_flags);
-    if (window == nullptr)
+static int ctxErrorHandler(Display* dpy, XErrorEvent* ev)
+{
+    g_ctxErrorOccurred = true;
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    g_display = XOpenDisplay(NULL);
+
+    if (!g_display)
     {
-        printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
-        return 1;
+        printf("Failed to open X g_display\n");
+        exit(1);
     }
 
-    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-    SDL_GL_MakeCurrent(window, gl_context);
-    SDL_GL_SetSwapInterval(1); // Enable vsync
+    int glx_major, glx_minor;
+
+    // FBConfigs were added in GLX version 1.3.
+    if (!glXQueryVersion(g_display, &glx_major, &glx_minor) ||
+        ((glx_major == 1) && (glx_minor < 3)) || (glx_major < 1))
+    {
+        printf("Invalid GLX version");
+        exit(1);
+    }
+
+    FindBestFrameBuffer();
+    CreateX11Window();
+    CreateGLXContext();
+
+    if (g_ctxErrorOccurred || !g_glxContext)
+    {
+        printf("Failed to create an OpenGL context\n");
+        exit(1);
+    }
+
+    // Verifying that context is a direct context
+    if (!glXIsDirect(g_display, g_glxContext))
+    {
+        printf("Indirect GLX rendering context obtained\n");
+    }
+    else
+    {
+        printf("Direct GLX rendering context obtained\n");
+    }
+
+    printf("Making context current\n");
+    glXMakeCurrent(g_display, g_window, g_glxContext);
+
+    // Initialize OpenGL loader
+#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+    bool err = gl3wInit() != 0;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+    bool err = glewInit() != GLEW_OK;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+    bool err = gladLoadGL() == 0;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
+    bool err = gladLoaderLoadGL() == 0; // glad2 recommend using the windowing library loader instead of the (optionally) bundled one.
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
+    bool err = false;
+    glbinding::Binding::initialize();
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
+    bool err = false;
+    glbinding::initialize([](const char* name) { return (glbinding::ProcAddress)glfwGetProcAddress(name); });
+#else
+    bool err = false; // If you use IMGUI_IMPL_OPENGL_LOADER_CUSTOM, your loader is likely to requires some form of initialization.
+#endif
+    if (err)
+    {
+        fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+        return 1;
+    }
 
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO &io = ImGui::GetIO();
-
-    (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
-
-    ImFont *font = io.Fonts->AddFontFromFileTTF("/home/viacheslav/Documents/Project/kkt_firmware/resources/fonts/DejaVuSansMono (1)/DejaVuSansMono.ttf", 16.0f, nullptr,
-                                                io.Fonts->GetGlyphRangesCyrillic());
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
-    // ImGui::StyleColorsLight();
-
-    // Setup scaling
-    // ImGuiStyle& style = ImGui::GetStyle();
-    // style.ScaleAllSizes(main_scale);        // Bake a fixed style scale. (until we have
-    // a solution for dynamic style scaling, changing this requires resetting Style +
-    // calling this again) style.FontScaleDpi = main_scale;        // Set initial font
-    // scale. (using io.ConfigDpiScaleFonts=true makes this unnecessary. We leave both
-    // here for documentation purpose)
+    //ImGui::StyleColorsClassic();
 
     // Setup Platform/Renderer backends
-    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
-    ImGui_ImplOpenGL2_Init();
+    ImGui_ImplOpenGL3_Init();
+    ImGui_ImplX11_Init(g_display, (void*)g_window);
+
+    // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or g_display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'docs/FONTS.md' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+    io.Fonts->AddFontFromFileTTF("/home/viacheslav/Documents/Project/cpp_sandbox/imgui_test/resources/DejaVuSansMono.ttf",
+                                 16.0f, nullptr, io.Fonts->GetGlyphRangesCyrillic());
 
     // Our state
     bool show_demo_window = true;
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-    // Main loop
-    bool done = false;
-    while (!done)
+    XEvent event;
+    bool running = true;
+
+    while (running)
     {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if
-        // dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your
-        // main application, or clear/overwrite your copy of the mouse data.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to
-        // your main application, or clear/overwrite your copy of the keyboard data.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your
-        // application based on those two flags.
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
+        //if (XEventsQueued(g_display, QueuedAlready) > 0)
+        //if (XQLength() > 0)
+        if (XPending(g_display) > 0)
         {
-            ImGui_ImplSDL2_ProcessEvent(&event);
-            if (event.type == SDL_QUIT)
-                done = true;
-            if (event.type == SDL_WINDOWEVENT &&
-                event.window.event == SDL_WINDOWEVENT_CLOSE &&
-                event.window.windowID == SDL_GetWindowID(window))
-                done = true;
+            XNextEvent(g_display, &event);
+            ImGui_ImplX11_EventHandler(event);
+
+            switch (event.type)
+            {
+            case Expose:
+                printf("Expose\n");
+                break;
+
+            case ClientMessage:
+                if (event.xclient.data.l[0] == g_wmDeleteMessage)
+                    running = false;
+                break;
+
+            default:
+                break;
+            }
         }
-        if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED)
+        else
         {
-            SDL_Delay(10);
-            continue;
+            glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            // Start the Dear ImGui frame
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplX11_NewFrame();
+            ImGui::NewFrame();
+
+            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+            if (show_demo_window)
+                ImGui::ShowDemoWindow(&show_demo_window);
+
+            // Rendering
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            glXSwapBuffers(g_display, g_window);
+
+            usleep(7000);
         }
-
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL2_NewFrame();
-        ImGui_ImplSDL2_NewFrame();
-        ImGui::NewFrame();
-
-        // 1. Show the big demo window (Most of the sample code is in
-        // ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear
-        // ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
-        // Rendering
-        ImGui::Render();
-        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w,
-                     clear_color.z * clear_color.w, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        // glUseProgram(0); // You may want this if using this code in an OpenGL 3+
-        // context where shaders may be bound
-        ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
-        SDL_GL_SwapWindow(window);
     }
 
-    // Cleanup
-    ImGui_ImplOpenGL2_Shutdown();
-    ImGui_ImplSDL2_Shutdown();
-    ImGui::DestroyContext();
-
-    SDL_GL_DeleteContext(gl_context);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    DestroyGLXContext();
+    DestroyX11Window();
 
     return 0;
+}
+
+void FindBestFrameBuffer()
+{
+    // Get a matching FB config
+    static int visual_attribs[] = {
+        GLX_X_RENDERABLE    , True,
+        GLX_DRAWABLE_TYPE   , GLX_WINDOW_BIT,
+        GLX_RENDER_TYPE     , GLX_RGBA_BIT,
+        GLX_X_VISUAL_TYPE   , GLX_TRUE_COLOR,
+        GLX_RED_SIZE        , 8,
+        GLX_GREEN_SIZE      , 8,
+        GLX_BLUE_SIZE       , 8,
+        GLX_ALPHA_SIZE      , 8,
+        GLX_DEPTH_SIZE      , 24,
+        GLX_STENCIL_SIZE    , 8,
+        GLX_DOUBLEBUFFER    , True,
+        //GLX_SAMPLE_BUFFERS  , 1,
+        //GLX_SAMPLES         , 4,
+        None
+    };
+
+    printf("Getting matching framebuffer configs\n");
+    int fbcount;
+    GLXFBConfig* fbc = glXChooseFBConfig(g_display, DefaultScreen(g_display), visual_attribs, &fbcount);
+    if (!fbc)
+    {
+        printf("Failed to retrieve a framebuffer config\n");
+        exit(1);
+    }
+    printf("Found %d matching FB configs.\n", fbcount);
+
+    // Pick the FB config/visual with the most samples per pixel
+    printf("Getting XVisualInfos\n");
+    int best_fbc = -1, worst_fbc = -1, best_num_samp = -1, worst_num_samp = 999;
+
+    int i;
+    for (i = 0; i < fbcount; ++i)
+    {
+        XVisualInfo* vi = glXGetVisualFromFBConfig(g_display, fbc[i]);
+        if (vi)
+        {
+            int samp_buf, samples;
+            glXGetFBConfigAttrib(g_display, fbc[i], GLX_SAMPLE_BUFFERS, &samp_buf);
+            glXGetFBConfigAttrib(g_display, fbc[i], GLX_SAMPLES, &samples);
+
+            printf("  Matching fbconfig %d, visual ID 0x%2lx: SAMPLE_BUFFERS = %d,"
+                " SAMPLES = %d\n",
+                i, vi->visualid, samp_buf, samples);
+
+            if (best_fbc < 0 || (samp_buf && samples > best_num_samp))
+                best_fbc = i, best_num_samp = samples;
+            if (worst_fbc < 0 || !samp_buf || samples < worst_num_samp)
+                worst_fbc = i, worst_num_samp = samples;
+        }
+        XFree(vi);
+    }
+
+    g_bestFbConfig = fbc[best_fbc];
+
+    // Be sure to free the FBConfig list allocated by glXChooseFBConfig()
+    XFree(fbc);
+}
+
+void CreateX11Window()
+{
+    // Get a visual
+    XVisualInfo* vi = glXGetVisualFromFBConfig(g_display, g_bestFbConfig);
+    printf("Chosen visual ID = 0x%lx\n", vi->visualid);
+
+    printf("Creating colormap\n");
+    XSetWindowAttributes swa;
+    swa.colormap = g_colorMap = XCreateColormap(g_display,
+        RootWindow(g_display, vi->screen),
+        vi->visual, AllocNone);
+    swa.background_pixmap = None;
+    swa.border_pixel = 0;
+    swa.event_mask = StructureNotifyMask;
+
+    printf("Creating window\n");
+    g_window = XCreateWindow(g_display, RootWindow(g_display, vi->screen),
+        0, 0, 230, 420, 0, vi->depth, InputOutput,
+        vi->visual,
+        CWBorderPixel | CWColormap | CWEventMask, &swa);
+
+    if (!g_window)
+    {
+        printf("Failed to create window.\n");
+        exit(1);
+    }
+
+    // Done with the visual info data
+    XFree(vi);
+
+    XStoreName(g_display, g_window, "GL 3.0 Window");
+
+    printf("Mapping window\n");
+    XMapWindow(g_display, g_window);
+
+    g_wmDeleteMessage = XInternAtom(g_display, "WM_DELETE_WINDOW", False);
+    XSetWMProtocols(g_display, g_window, &g_wmDeleteMessage, 1);
+
+    XSelectInput(g_display,
+        g_window,
+        SubstructureRedirectMask | SubstructureNotifyMask |
+        KeyPressMask | KeyReleaseMask |
+        ButtonPressMask | ButtonReleaseMask);
+
+    // Запрещаем изменение размера
+    XSizeHints hints;
+    hints.flags = PMinSize | PMaxSize;
+    hints.min_width = hints.max_width = 230;
+    hints.min_height = hints.max_height = 420;
+    XSetWMNormalHints(g_display, g_window, &hints);
+}
+
+void CreateGLXContext()
+{
+    // Get the default screen's GLX extension list
+    const char* glxExts = glXQueryExtensionsString(g_display,
+        DefaultScreen(g_display));
+
+    // NOTE: It is not necessary to create or make current to a context before
+    // calling glXGetProcAddressARB
+    glXCreateContextAttribsARBProc glXCreateContextAttribsARB = 0;
+    glXCreateContextAttribsARB = (glXCreateContextAttribsARBProc)
+        glXGetProcAddressARB((const GLubyte*)"glXCreateContextAttribsARB");
+
+
+
+    // Install an X error handler so the application won't exit if GL 3.0
+    // context allocation fails.
+    //
+    // Note this error handler is global.  All g_display connections in all threads
+    // of a process use the same error handler, so be sure to guard against other
+    // threads issuing X commands while this code is running.
+    g_ctxErrorOccurred = false;
+    int (*oldHandler)(Display*, XErrorEvent*) =
+        XSetErrorHandler(&ctxErrorHandler);
+
+    // Check for the GLX_ARB_create_context extension string and the function.
+    // If either is not present, use GLX 1.3 context creation method.
+    if (!isExtensionSupported(glxExts, "GLX_ARB_create_context") ||
+        !glXCreateContextAttribsARB)
+    {
+        printf("glXCreateContextAttribsARB() not found"
+            " ... using old-style GLX context\n");
+        g_glxContext = glXCreateNewContext(g_display, g_bestFbConfig, GLX_RGBA_TYPE, 0, True);
+    }
+    // If it does, try to get a GL 3.0 context!
+    else
+    {
+        int context_attribs[] =
+        {
+          GLX_CONTEXT_MAJOR_VERSION_ARB, 3,
+          GLX_CONTEXT_MINOR_VERSION_ARB, 0,
+          //GLX_CONTEXT_FLAGS_ARB        , GLX_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB,
+          None
+        };
+
+        printf("Creating context\n");
+        g_glxContext = glXCreateContextAttribsARB(g_display, g_bestFbConfig, 0,
+            True, context_attribs);
+
+        // Sync to ensure any errors generated are processed.
+        XSync(g_display, False);
+        if (!g_ctxErrorOccurred && g_glxContext)
+            printf("Created GL 3.0 context\n");
+        else
+        {
+            // Couldn't create GL 3.0 context.  Fall back to old-style 2.x context.
+            // When a context version below 3.0 is requested, implementations will
+            // return the newest context version compatible with OpenGL versions less
+            // than version 3.0.
+            // GLX_CONTEXT_MAJOR_VERSION_ARB = 1
+            context_attribs[1] = 1;
+            // GLX_CONTEXT_MINOR_VERSION_ARB = 0
+            context_attribs[3] = 0;
+
+            g_ctxErrorOccurred = false;
+
+            printf("Failed to create GL 3.0 context"
+                " ... using old-style GLX context\n");
+            g_glxContext = glXCreateContextAttribsARB(g_display, g_bestFbConfig, 0,
+                True, context_attribs);
+        }
+    }
+
+    // Sync to ensure any errors generated are processed.
+    XSync(g_display, False);
+
+    // Restore the original error handler
+    XSetErrorHandler(oldHandler);
+}
+
+void DestroyGLXContext()
+{
+    glXMakeCurrent(g_display, 0, 0);
+    glXDestroyContext(g_display, g_glxContext);
+}
+
+void DestroyX11Window()
+{
+    XDestroyWindow(g_display, g_window);
+    XFreeColormap(g_display, g_colorMap);
+    XCloseDisplay(g_display);
 }
